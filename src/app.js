a00fb9f3b1964e94af76f6b3bd2d45f129662088
untoldai <vishalp180705@gmail.com>
2024-10-18 23:53:10 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,14 +10,12 @@ const app = express();
 
 
 
-
 app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN : '*', // Fallback to '*' if not set
-        credentials: true,
-    })
+  cors({
+    origin: "*",
+    credentials: true,
+  })
 );
-
 //this is middleware that is use to request json payload with limit
 app.use(express.json({ limit: "16kb" }));
 
